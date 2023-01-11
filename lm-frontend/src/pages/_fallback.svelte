521bf58ffab1f3726ca3367771f8d6c6248e5d17
Jaime <jaime.munoz@seqera.io>
2023-01-11 23:19:29 +0100

    FE: 404 page

---
+++ lm-frontend/src/pages/_fallback.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { leftover } from "@roxi/routify";
+</script>
+
+<div class="
+    fixed h-full w-full 
+    grid place-content-center gap-8
+    bg-slate-800
+    text-white text-center font-extrabold
+">
+    <p class="text-8xl">404</p>
+    <p class="text-4xl">'/{$leftover}' not found </p>
+</div>
