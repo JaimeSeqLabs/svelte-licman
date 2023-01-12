a607bf8fd949142f9cc923e62614de22174400fa
Jaime <jaime.munoz@seqera.io>
2023-01-12 02:27:07 +0100

    FE: fix 404

--- lm-frontend/src/pages/_fallback.svelte
+++ lm-frontend/src/pages/_fallback.svelte
@@ -3,10 +3,8 @@
 </script>
 
 <div class="
-    fixed h-full w-full 
     grid place-content-center gap-8
-    bg-slate-800
-    text-white text-center font-extrabold
+    text-center font-extrabold
 ">
     <p class="text-8xl">404</p>
     <p class="text-4xl">'/{$leftover}' not found </p>
