2758609723202889a795c799961f61f97fe7c0d8
Jaime <jaime.munoz@seqera.io>
2023-01-13 01:28:11 +0100

    FE: improved login layout

--- lm-frontend/src/pages/login/index.svelte
+++ lm-frontend/src/pages/login/index.svelte
@@ -1,13 +1,25 @@
 <script lang="ts">
-    import { Column, FluidForm, Grid, PasswordInput, Row, TextInput } from "carbon-components-svelte";
-    import SeqeraLogo from "../../lib/components/SeqeraLogo.svelte";
-
+    import { Button, FluidForm, PasswordInput, TextInput, Tile } from "carbon-components-svelte";
+    import { Login } from "carbon-icons-svelte";
+    import logo from "/seqera_logo_white.png"
 
 </script>
 
+<style>
+    img {
+        @apply p-4;
+        opacity: 1;
+        background-color: #0f62fe;
+    }
+</style>
+
 <div class="h-full flex flex-wrap justify-center content-center">
-    <div class="w-2/5">
-        <SeqeraLogo/>
+    <div class="w-96">
+
+        <img src={logo} alt="">
+
+        <Tile class="text-xl font-bold">Staff access</Tile>
+            
         <FluidForm>
             <TextInput
                 required
@@ -21,5 +33,12 @@
                 placeholder="Enter password..."
             />
         </FluidForm>
+        
+        <Tile>
+            <div class="flex justify-end">
+                <Button size="field" icon={Login} iconDescription="Login">Login</Button>
+            </div>
+        </Tile>
+
     </div>
 </div>
