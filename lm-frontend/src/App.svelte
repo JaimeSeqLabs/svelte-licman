ddb03d2157384975743a490e1f4c225a4bfb52c9
Jaime <jaime.munoz@seqera.io>
2023-02-08 00:04:55 +0100

    FE: cleanup, single theme css

--- lm-frontend/src/App.svelte
+++ lm-frontend/src/App.svelte
@@ -2,13 +2,8 @@
   import { Router } from "@roxi/routify";
   import { routes } from "../.routify/routes";
 
-  import "carbon-components-svelte/css/all.css"
-  import { Theme } from "carbon-components-svelte";
-  
-  
-  export let theme: any = "g80" // "white" | "g10" | "g80" | "g90" | "g100"
+  import "carbon-components-svelte/css/g80.css"
 
 </script>
 
-<Theme bind:theme/>
 <Router {routes}/>
