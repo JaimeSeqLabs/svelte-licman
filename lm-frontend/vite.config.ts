4f89600c74a40ef08a68c56d42d268009dc37282
Jaime <jaime.munoz@seqera.io>
2023-02-27 19:14:53 +0100

    FE: vite proxy to BE

--- lm-frontend/vite.config.ts
+++ lm-frontend/vite.config.ts
@@ -4,4 +4,14 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  server: {
+    proxy: {
+      '/lm': {
+        target: 'http://localhost:9090',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/lm/, "")
+      }
+    }
+  }
 })
